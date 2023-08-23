00143c7be4031325a57319a422b0f5b7f02b26e7
truong10css <113449308+truong10css@users.noreply.github.com>
2023-08-23 09:01:08 +0700

    Update dragon.php

--- dragon.php
+++ dragon.php
@@ -10,7 +10,7 @@ echo "\033[1;92m
 ╚═╝░░░░░╚═╝░░░╚═╝░░░░░░╚═╝░░░\n";
 
 echo "\033[1;96m[1] Cài đặt JDK 17\n";
-echo "\033[1;932m[2] Giải nén tệp zip\n";
+echo "\033[1;932m[2] Giải nén tệp src.zip từ URL\n";
 echo "\033[1;92m[3] Chạy server\n";
 echo "\033[1;92mLua chon: \033[1;33m";
 
@@ -23,12 +23,12 @@ if ($luachon == 1) {
     sleep(1);
 } else if ($luachon == 2) {
     sleep(1);
-    system('unzip src.zip && clear ');
+    system('wget -O src.zip https://www.mediafire.com/file/aqvfeqk61qhutrm/nro1.rar/file && unzip src.zip && clear ');
     system('rm -rf src.zip');
     system('clear');
 } else if ($luachon == 3) {
     sleep(1);
-    system('java -Xms2G -Xmx2G -jar dist/mad.jar');
+    system(' java -server -jar dist/Heroes_Z.jar');
 } else {
     echo "\033[1;91mChon Sai Vui Lòng Chọn Lại\n";
     echo "\033[1;93m-------------------------------------------------------------\n";
